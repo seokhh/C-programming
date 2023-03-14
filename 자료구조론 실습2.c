0e97b593f95dbec3e05787e994c9e4ce0ee9e8f6
seokhh <119908334+seokhh@users.noreply.github.com>
2023-03-14 16:12:21 +0900

    Add files via upload

---
+++ 자료구조론 실습2.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int numbers[100];
+int size;
+
+void input_list() {
+	for (int i = 0; i < size; ++i) {
+		numbers[i] = rand() % 100;
+	}
+}
+void print_list() {
+	for (int i = 0; i < size; ++i) {
+		printf("%3d,", numbers[i]);
+		if (i % 8 == 7)
+			printf("\n");
+	}
+}
+
+void add_list(int val) {
+	printf("값 %d를 리스트 맨 끝에 추가합니다.\n\n", val);
+	numbers[size++] = val;
+}
+void insert_list(int pos, int val) {
+	printf("값 %d를 %d 번째에 삽입합니다.\n\n", val, pos);
+	for (int k = size - 1; k >= pos; k--)
+		numbers[k + 1] = numbers[k];
+	numbers[pos] = val;
+	size++;
+}
+int delete_list(int pos) {
+	int result = numbers[pos];
+	for (int k = pos; k < size - 1; k++)
+		numbers[k] = numbers[k + 1];
+	printf("%d 번째 값을 삭제합니다. 값 = %d\n\n", pos, result);
+	size--;
+	return result;
+}
+int update_list(int pos, int val) {
+	int result = numbers[pos];
+	numbers[pos] = val;
+	printf("%d 번째 값을 %d로 변경합니다. 이전값 = %d\n\n", pos, val, result);
+	return result;
+}
+
+
+void run_menu() {
+	int pos, val;
+	int menu;
+	while (1) {
+		printf("\n(1) 끝에 값 추가 \n(2) pos번째에 값 추가\n");
+		printf("(3) pos번째 삭제 \n(4) pos번째 값 변경 \n(5) 종료 ");
+		printf("\n 메뉴선택 => ");
+		scanf_s("%d", &menu);
+		if (menu >= 5) break;
+		switch (menu) {
+		case 1:
+
+			scanf_s("%d", &val);
+			add_list(val);
+
+			break;
+		case 2:
+			scanf_s("%d", &pos);
+			scanf_s("%d", &val);
+			insert_list(pos, val);
+
+			break;
+		case 3:
+			scanf_s("%d", &pos);
+			delete_list(pos);
+			break;
+		case 4:
+			scanf_s("%d", &pos);
+			scanf_s("%d", &val);
+			update_list(pos, val);
+			break;
+		default: printf("종료");
+			break;
+		}
+		print_list();
+	}
+
+}
+void main() {
+	printf("숫자를 입력하세요: ");
+	scanf_s("%d", &size);
+	srand(1);
+	input_list();
+	run_menu();
+
+	return 0;
+}
